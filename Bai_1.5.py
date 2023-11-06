0df25f678886e57b840114cab94d0d4dfae50c60
NguyenPhThao23174600016 <148135338+NguyenPhThao23174600016@users.noreply.github.com>
2023-11-06 21:51:02 +0700

    Chuong 6

---
+++ Bai_1.5.py
@@ -0,0 +1,10 @@
+Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #bai_1.5:tinh tien hang
+>>> so_luong=eval(input("Nhap so luong : "))
+Nhap so luong : 100
+>>> don_gia=eval(input("Nhap don gia : "))
+Nhap don gia : 5000
+>>> thanh_tien=so_luong*don_gia
+>>> print("thanh tien : ",thanh_tien)
+thanh tien :  500000
