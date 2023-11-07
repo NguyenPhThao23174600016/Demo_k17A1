3a17df941358889a22826d095a46f688108ef2d3
NguyenPhThao23174600016 <148135338+NguyenPhThao23174600016@users.noreply.github.com>
2023-11-07 22:30:07 +0700

    Chuong 7

---
+++ Bai_7.1.py
@@ -0,0 +1,8 @@
+Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #bai7.1:tinh gia tri bieu thuc
+>>> x=eval(input("Nhap x:"))
+Nhap x:6
+>>> S= 1 + x + x**3/3 + x**5/5
+>>> print("S=",S)
+S= 1634.2
