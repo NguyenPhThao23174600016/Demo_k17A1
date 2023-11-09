84c78fa467d44cd49f29f95991f8f4b410fe164f
NguyenPhThao23174600016 <148135338+NguyenPhThao23174600016@users.noreply.github.com>
2023-11-09 13:06:09 +0700

    Delete Bai_1.7.py

--- Bai_1.7.py
+++
@@ -1,8 +0,0 @@
-Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> #bai_1.7:doi nhiet do tu do C sang do F
->>> Do_C=eval(input("Nhap do C : "))
-Nhap do C : 20
->>> Do_F=9/5*Do_C+32
->>> print("Do F:",Do_F)
-Do F: 68.0
